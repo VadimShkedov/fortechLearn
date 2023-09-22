86ff276adf70b709a8cd386ca04ab6529ee90329
KARWEST <vadimskedov@gmail.com>
2023-09-22 12:40:11 +0300

    task11

---
+++ task11.js
@@ -0,0 +1,23 @@
+const input = [5, 2, 8, 4, 8, 2, 5, 8, 2, 17, 8, 4, 2, 4, 7, 3]
+
+function sortAndSet(array) {
+    let countNums = {}
+    let sortedArray = array.sort((a, b) => a - b)
+    let finalArray = []
+
+    for (let elem of sortedArray) {
+        if (!countNums[elem]) {
+            countNums[elem] = 1
+        }
+        else {
+            countNums[elem] += 1
+        }
+    }
+    for (let key in countNums) {
+        if (countNums[key] == 1) {
+            finalArray.push(parseInt(key))
+        }
+    }
+    return finalArray
+}
+console.log(sortAndSet(input));
