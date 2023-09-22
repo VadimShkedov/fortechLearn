0f2d2729866473f905a0ffba3066a2e582068f70
KARWEST <vadimskedov@gmail.com>
2023-09-22 12:53:08 +0300

    task13

--- task12.js
+++
@@ -1,8 +0,0 @@
-function makeArrayPair(obj) {
-    let finalArray = []
-    for (let key in obj) {
-        finalArray.push([key, obj[key]])
-    }
-    return finalArray
-}
-console.log(makeArrayPair({ 'dog': 6, 'cat': 11 }));
---
+++ task13.js
@@ -0,0 +1,14 @@
+function multiplyNums(obj) {
+    for (let key in obj) {
+        if (!isNaN(obj[key])) {
+            obj[key] *= 2
+        }
+    }
+    return obj
+}
+console.log(multiplyNums({
+    name: 'test',
+    age: 25,
+    weight: 65,
+    height: 165
+}));
