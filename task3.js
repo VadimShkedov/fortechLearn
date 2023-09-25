85728d8b4d0a1f3d2de00b723d0a6e7ce24d534f
VadimShkedov <shkedov.fortech@gmail.com>
2023-09-25 12:14:58 +0300

    task3.1.1

--- task3.js
+++ task3.js
@@ -1,18 +1,21 @@
-const removeDuplicates = arr => { //возвращает уникальные элементы в массиве
-    let resultArr = []
-    for (let i = 0; i < arr.length; i++) {
-        let isExist = false
-        const stringValue = arr[i].toString()
-        for (let j = 0; j < resultArr.length; j++) {
-            if (resultArr[j] == stringValue.toLowerCase()) {
-                isExist = true
-                break
-            }
-        }
-        if (!isExist) {
-            resultArr.push(stringValue.toLowerCase())
-        }
+const removeDuplicates = (arr) => { //возвращает уникальные элементы в массиве
+  let resultArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let isExist = false;
+    const stringValue = arr[i].toString();
+
+    for (let j = 0; j < resultArr.length; j++) {
+      if (resultArr[j] == stringValue.toLowerCase()) {
+        isExist = true;
+        break;
+      }
     }
-    return resultArr
+
+    if (!isExist) {
+      resultArr.push(stringValue.toLowerCase());
+    }
+  }
+  return resultArr;
 }
+
 console.log(removeDuplicates(['text', 'education', 'part', 'Text']));
