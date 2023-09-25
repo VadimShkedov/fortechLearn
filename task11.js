dc39bfec395dd1ecec6a61b4a2e7513e13ba6854
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-09-25 16:16:13 +0300

    Update task11.js

--- task11.js
+++ task11.js
@@ -1,23 +1,24 @@
-const input = [5, 2, 8, 4, 8, 2, 5, 8, 2, 17, 8, 4, 2, 4, 7, 3]
+const input = [5, 2, 8, 4, 8, 2, 5, 8, 2, 17, 8, 4, 2, 4, 7, 3];
 
-function sortAndSet(array) {
-    let countNums = {}
-    let sortedArray = array.sort((a, b) => a - b)
-    let finalArray = []
+const sortAndSet = (array) => {
+    let countNums = {};
+    let finalArray = [];
+    let sortedArray = array.sort((a, b) => a - b);
 
     for (let elem of sortedArray) {
         if (!countNums[elem]) {
-            countNums[elem] = 1
+            countNums[elem] = 1;
         }
         else {
-            countNums[elem] += 1
+            countNums[elem] += 1;
         }
     }
     for (let key in countNums) {
         if (countNums[key] == 1) {
-            finalArray.push(parseInt(key))
+            finalArray.push(parseInt(key));
         }
     }
-    return finalArray
+    return finalArray;
 }
+
 console.log(sortAndSet(input));
