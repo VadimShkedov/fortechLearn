b492bc72314a6af178458c6f1a4390ff7d5e1aef
VadimShkedov <shkedov.fortech@gmail.com>
2023-09-25 12:30:25 +0300

    task2.1

--- task2.js
+++ task2.js
@@ -3,24 +3,26 @@ const array2 = [1, 4, 2, 4]
 const array3 = [6, 3, 2, 8, 1]
 
 const giveUniqueElements = (mainArray, ...arrs) => { //возвращает уникальные элементы в нескольких массивах
-    const settedAnotherArrays = []
+  const uniqueAnotherArrays = [];
 
-    arrs.forEach((value) => settedAnotherArrays.push(new Set(value)))
-    let finalArray = []
+  arrs.forEach((value) => uniqueAnotherArrays.push(new Set(value)));
+  let finalArray = [];
 
-    mainArray.forEach((valueMainArray) => {
-        let countRepeatElements = 0
-        for (let anotherArray of settedAnotherArrays) {
-            anotherArray.forEach((valueAnotherArray) => {
-                if (valueAnotherArray == valueMainArray) {
-                    countRepeatElements++
-                }
-            })
+  mainArray.forEach((valueMainArray) => {
+    let countRepeatElements = 0;
+    for (let anotherArray of uniqueAnotherArrays) {
+      anotherArray.forEach((valueAnotherArray) => {
+        if (valueAnotherArray === valueMainArray) {
+          countRepeatElements++;
         }
-        if (countRepeatElements == settedAnotherArrays.length) {
-            finalArray.push(valueMainArray)
-        }
-    })
-    return finalArray
+      })
+    }
+
+    if (countRepeatElements === uniqueAnotherArrays.length) {
+      finalArray.push(valueMainArray);
+    }
+  })
+  return finalArray;
 }
+
 console.log(giveUniqueElements(array1, array2, array3));
