512b93a279040a57c89d6452dac479c293aac0f6
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-09-25 16:17:11 +0300

    Update task12.js

--- task12.js
+++ task12.js
@@ -1,8 +1,9 @@
-function makeArrayPair(obj) {
-    let finalArray = []
+const makeArrayPair = (obj) => {
+    let finalArray = [];
     for (let key in obj) {
-        finalArray.push([key, obj[key]])
+        finalArray.push([key, obj[key]]);
     }
-    return finalArray
+    return finalArray;
 }
+
 console.log(makeArrayPair({ 'dog': 6, 'cat': 11 }));
