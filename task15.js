c10273257984257884edab0935477877ff315584
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-09-25 16:23:04 +0300

    Update task15.js

--- task15.js
+++ task15.js
@@ -1,15 +1,15 @@
 const a = { test: 8, text: 9 };
 const b = { test: 8, text: 9 };
 
-
-function func(obj1, obj2) {
-    let isEqual = true
+function isObjectsEqual(obj1, obj2) {
+    let isEqual = true;
+    
     for (let key in obj1) {
         if (obj1[key] != obj2[key]) {
-            isEqual = false
+            isEqual = false;
         }
     }
-    return isEqual
+    return isEqual;
 }
 
-console.log(func(a, b));
+console.log(isObjectsEqual(a, b));
