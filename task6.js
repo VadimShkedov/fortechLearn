ca22946c42bd83bf074df0d1ee6c6529bd180b6d
VadimShkedov <shkedov.fortech@gmail.com>
2023-09-25 15:41:13 +0300

    task6.1

--- task6.js
+++ task6.js
@@ -1,21 +1,11 @@
-function func(num) {
-    if (num % 2 == 0) {
-        return (numSecond) => {
-            const resultNum = Math.pow(num, 2) * numSecond
-            return (numLast) => {
-                const sqrtSumLast = Math.sqrt(resultNum + numLast)
-                return Math.trunc(sqrtSumLast * 100) / 100
-            }
-        }
+const func = (num) => {
+  return (numSecond) => {
+    const resultNum = (num % 2 === 0) ? (num**2) * numSecond : (num**3) * numSecond;
+    return (numLast) => {
+      const sqrtSumLast = Math.sqrt(resultNum + numLast);
+      return Math.trunc(sqrtSumLast * 100) / 100;
     }
-    else {
-        return (numSecond) => {
-            const resultNum = Math.pow(num, 3) * numSecond
-            return (numLast) => {
-                const sqrtSumLast = Math.sqrt(resultNum + numLast)
-                return Math.trunc(sqrtSumLast * 100) / 100
-            }
-        }
-    }
+  }
 }
-console.log(func(17)(6)(2))
+
+console.log(func(17)(6)(2));
