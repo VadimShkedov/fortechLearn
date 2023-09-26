8f7ca8a981a27b2ca3b8b56e2d9f189adcc64258
VadimShkedov <shkedov.fortech@gmail.com>
2023-09-26 10:49:12 +0300

    task14.1

--- task14.js
+++ task14.js
@@ -1,30 +1,34 @@
 const getMaxMinString = (string1, string2) => {
-    if (string1.length > string2.length) {
-        return {
-            maxStr: string1,
-            minStr: string2
-        }
-    }
+  if (string1.length > string2.length) {
+
     return {
-        maxStr: string2,
-        minStr: string1
+      maxStr: string1,
+      minStr: string2
     }
+  }
+
+  return {
+    maxStr: string2,
+    minStr: string1
+  }
 }
 
 const searchingSubstring = (string1, string2) => {
-    let isStringExist = false;
-    const { minStr, maxStr } = getMaxMinString(string1, string2);
+  let isStringExist = false;
+  
+  const { minStr, maxStr } = getMaxMinString(string1, string2);
 
-    for (let i = 0; i < maxStr.length; i++) {
-        const sliceStr = maxStr.slice(i, i + minStr.length);
-        if (sliceStr == minStr) {
-            isStringExist = true;
-            break;
-        }
+  for (let i = 0; i < maxStr.length; i++) {
+    const sliceStr = maxStr.slice(i, i + minStr.length);
+    if (sliceStr == minStr) {
+      isStringExist = true;
+      break;
     }
-    return isStringExist;
+  }
+
+  return isStringExist;
 }
 
-console.log(searchSubstring('text education part 2', 'text'));
+console.log(searchingSubstring('text education part 2', 'text'));
 
-console.log(searchSubstring('text education part 2', 'test'));
+console.log(searchingSubstring('text education part 2', 'test'));
