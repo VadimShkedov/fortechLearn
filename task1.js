c4314da5eff6f4102e26b0152e5879bcba36b65b
VadimShkedov <shkedov.fortech@gmail.com>
2023-09-29 16:29:23 +0300

    task1.2

--- task1.js
+++ task1.js
@@ -2,20 +2,20 @@ const text1 = 'text education part 2';
 const text2 = 'education department fortech';
 
 const giveDiffSymbols = (str1, str2) => {
-    let resultStr = ''
-    let maxStr = ''
-    let minStr = ''
+    let resultStr = '';
+    let maxStr = '';
+    let minStr = '';
 
     if (str1.length > str2.length) {
-        maxStr = str1
-        minStr = str2
+        maxStr = str1;
+        minStr = str2;
     } else {
-        maxStr = str2
-        minStr = str1
+        maxStr = str2;
+        minStr = str1;
     }
 
     for (let i = minStr.length; i < maxStr.length; i++) {
-        resultStr += maxStr[i]
+        resultStr += maxStr[i];
     }
 
     return resultStr;
