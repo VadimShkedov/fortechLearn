c475589cb02dbcce8fcf7fd3ed91fdf4a40ad7f1
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-10-02 15:37:03 +0300

    task3.1.js

--- task3.js
+++ task3.js
@@ -1,12 +1,19 @@
-const removeDuplicates = (array) => { //возвращает уникальные элементы в массиве task3
+const removeDuplicates = (array) => { //возвращает уникальные элементы в массиве
   const resultObj = {};
+  const resultArr = [];
 
   for (const value of array) {
     const key = (typeof value === 'string') ? value.toLowerCase() : value;
     isNaN(resultObj[key]) ? resultObj[key] = 1 : resultObj[key]++;
   }
 
-  return resultObj;
+  for (const key in resultObj) {
+    if (resultObj[key] === 1) {
+      resultArr.push(key)
+    }
+  }
+
+  return resultArr;
 }
 
 console.log(removeDuplicates([true, 'education', 'part', true]));
