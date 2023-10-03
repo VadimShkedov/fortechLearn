828c04e23e0ef64d723916906ade3927117ade6a
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-10-03 11:41:58 +0300

    task3.4.js

--- task3.js
+++ task3.js
@@ -2,9 +2,9 @@ const removeDuplicates = (array) => {
   const resultArr = [];
 
   for (const value of array) {
-    const neededValue = (typeof value === 'string') ? value.toLowerCase() : value;
-    if (!resultArr.includes(neededValue)) {
-      resultArr.push(neededValue);
+    const toLowerValue = (typeof value === 'string') ? value.toLowerCase() : value;
+    if (!resultArr.includes(toLowerValue)) {
+      resultArr.push(toLowerValue);
     }
   }
   
