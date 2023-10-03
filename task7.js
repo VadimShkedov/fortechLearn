3f30424f9573c344de74092a22a7c97435040504
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-10-03 12:13:08 +0300

    task7.2.js

--- task7.js
+++ task7.js
@@ -2,16 +2,17 @@ const string1 = 'testset';
 const string2 = 'abbcsa';
 
 const isPalindrome = (string) => {
-  const length = Math.trunc((string.length - 1) / 2)
-  let isPalindr = true
+  const length = Math.trunc((string.length - 1) / 2);
+  let isPalindr = true;
 
   for (let i = 0; i < length; i++) {
     if (string[i] !== string[string.length - i - 1]) {
-      isPalindr = false
-      break
+      isPalindr = false;
+      break;
     }
   }
-  return isPalindr
+  
+  return isPalindr;
 }
 
 console.log(isPalindrome(string1));
