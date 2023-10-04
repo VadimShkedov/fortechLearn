bafdf2dd492dd09adcf9125c5efb5c0b9b3b9183
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-10-04 17:07:25 +0300

    Update task6.js

--- task6.js
+++ task6.js
@@ -1,11 +1,11 @@
-const func = (num) => {
-  return (numSecond) => {
-    const resultNum = (num % 2 === 0) ? (num**2) * numSecond : (num**3) * numSecond;
-    return (numLast) => {
-      const sqrtSumLast = Math.sqrt(resultNum + numLast);
+const manipulateWithNums = (num) => {
+  return (secondNumber) => {
+    const result = (num % 2 === 0) ? (num**2) : (num**3);
+    return (lastNumber) => {
+      const sqrtSumLast = Math.sqrt(result * secondNumber + lastNumber);
       return Math.trunc(sqrtSumLast * 100) / 100;
     }
   }
 }
 
-console.log(func(17)(6)(2));
+console.log(manipulateWithNums(17)(6)(2));
