4723c8358c7946c57362f0221ea98e0cc84a68a9
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-10-04 17:14:04 +0300

    Update task8.js

--- task8.js
+++ task8.js
@@ -1,13 +1,13 @@
 const deleteElements = (arrayElements, arrayIndexes) => {
-    const finalArray = [];
-    
-    for (let index = 0; index < arrayElements.length; index++) {
-        if (!arrayIndexes.includes(index)) {
-            finalArray.push(arrayElements[index]);
-        }
+  const finalArray = [];
+
+  for (let index = 0; index < arrayElements.length; index++) {
+    if (!arrayIndexes.includes(index)) {
+      finalArray.push(arrayElements[index]);
     }
-    
-    return finalArray;
+  }
+
+  return finalArray;
 }
 
 console.log(deleteElements([5, 2, 8, 6, 1, 9, 3, 6, 3, 7, 1], [2, 5, 1]));
