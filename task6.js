793722a5d956c1071c30c6c4e087846fd5b5a409
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-10-04 17:44:32 +0300

    refactor: update code

--- task6.js
+++ task6.js
@@ -1,7 +1,7 @@
 const manipulateWithNums = (num) => {
   const result = (num % 2 === 0) ? (num**2) : (num**3);
   return (secondNumber) => {
-    const multiplyNumbers = result * secondNumber
+    const multiplyNumbers = result * secondNumber;
     return (lastNumber) => {
       const sqrtSumLast = Math.sqrt(multiplyNumbers + lastNumber);
       return Math.trunc(sqrtSumLast * 100) / 100;
