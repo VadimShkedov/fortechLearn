6a8b45d89110508bea88f9fde3aebae6f4694382
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-10-04 17:30:11 +0300

    refactor: added new variable

--- task6.js
+++ task6.js
@@ -2,7 +2,8 @@ const manipulateWithNums = (num) => {
   return (secondNumber) => {
     const result = (num % 2 === 0) ? (num**2) : (num**3);
     return (lastNumber) => {
-      const sqrtSumLast = Math.sqrt(result * secondNumber + lastNumber);
+      const multiplyNumbers = result * secondNumber
+      const sqrtSumLast = Math.sqrt(multiplyNumbers + lastNumber);
       return Math.trunc(sqrtSumLast * 100) / 100;
     }
   }
