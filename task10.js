9b333cc7fb4353ed23515afed4c0ef84df6768e3
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-10-04 18:03:06 +0300

    refactor: update code

--- task10.js
+++ task10.js
@@ -1,19 +1,21 @@
 const existInFirst = (array1, array2) => {
-    let finalArray = [];
+  const finalArray = [];
 
-    for (let i = 0; i < array1.length; i++) {
-        let isNumExist = false;
-        for (let j = 0; j < array2.length; j++) {
-            if (array1[i] == array2[j]) {
-                isNumExist = true;
-                break;
-            }
-        }
-        if (!isNumExist) {
-            finalArray.push(array1[i]);
-        }
+  for (let i = 0; i < array1.length; i++) {
+    let isNumExist = false;
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] === array2[j]) {
+        isNumExist = true;
+        break;
+      }
     }
-    return finalArray;
+
+    if (!isNumExist) {
+      finalArray.push(array1[i]);
+    }
+  }
+
+  return finalArray;
 }
 
 console.log(existInFirst([4, 7, 2, 9, 3, 5, 6, 4, 5, 1, 4], [4, 5, 6, 7, 8]));
