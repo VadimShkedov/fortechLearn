d35d4ac789b32dd939f8e2c2b83f64e3bc76c222
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-10-06 11:03:50 +0300

    fix: added compare objects by length and create a loop on biggest of then

--- task15.js
+++ task15.js
@@ -1,10 +1,18 @@
 const object1 = { test: 8, text: 9 };
-const object2 = { test: '8', text: 9 };
+const object2 = { text: 9, test: 8, for: 12};
+
+const maxLengthObject = (obj1, obj2) => {
+  const lengthFirst = Object.keys(obj1).length;
+  const lengthSecond = Object.keys(obj2).length;
+
+  return (lengthFirst > lengthSecond) ? obj1 : obj2;
+}
 
 const checkEqualObjects = (obj1, obj2) => {
   let isObjectsEqual = true;
+  const maxObject = maxLengthObject(obj1, obj2)
 
-  for (let key in obj1) {
+  for (let key in maxObject) {
     if (obj1[key] !== obj2[key]) {
       isObjectsEqual = false;
       break;
