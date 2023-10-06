b71c7fa01a2793df8e7e733a5cddd64b72cb0a3c
VadimShkedov <145138500+VadimShkedov@users.noreply.github.com>
2023-10-06 10:17:08 +0300

    refactor: change code style

--- task9.js
+++ task9.js
@@ -1,25 +1,24 @@
 const objects = [
-    { name: 'test', age: 34, country: 'RF' },
-    { name: 'test2', age: 12, country: 'RF' },
-    { name: 'test1', age: 54, country: 'RF' }
+  { name: 'test', age: 34, country: 'RF' },
+  { name: 'test2', age: 12, country: 'RF' },
+  { name: 'test1', age: 54, country: 'RF' }
 ];
 
 const filtringCollection = (array, equalTo, moreThan) => {
-    const finalArray = [];
+  const finalArray = [];
 
-    array.forEach((object) => {
-        const tempArray = [];
-        
-        for (let key in object) {
-            tempArray.push(object[key]);
-        }
-        
-        if (tempArray.includes(equalTo) && tempArray.find(value => value > moreThan)) {
-            finalArray.push(object);
-        }
-    });
-    
-    return finalArray;
+  array.forEach((object) => {
+    const tempArray = [];
+    for (let key in object) {
+      tempArray.push(object[key]);
+    }
+
+    if (tempArray.includes(equalTo) && tempArray.find(value => value > moreThan)) {
+      finalArray.push(object);
+    }
+  });
+
+  return finalArray;
 }
 
 console.log(filtringCollection(objects, 'RF', 18));
